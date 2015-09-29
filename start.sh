379f503d3ce25a169926476f2a92cc0c7deb9105
Luciano Resende <lresende@apache.org>
2015-09-29 14:35:23 -0700

    Auto-discover Spark configuration
    Utilizes SPARK_HOME system variable to identify
    where Spark is installed.
    
    Discover Spark master url utilizing hostname utility.

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 # Current dir
 CURDIR="$(cd "`dirname "$0"`"; pwd)"
 
-# using spark 1.5.0
-export SPARK_HOME="/Users/barbaragomes/opt/spark-1.5.0-bin-hadoop2.6"
+# using environment variable to find Spark home directory
+if [ -z "$SPARK_HOME" ]; then echo "SPARK_HOME is NOT set"; else echo "SPARK_HOME defined as '$SPARK_HOME'"; fi
 
-# generates the new .jar considering new configurations. 
+# generates the new .jar considering new configurations.
 # Run this command separated on cluster, before push code to all nodes. Comment it out on cluster
 echo " ==========  Compiling code and generating .jar ============"
 sbt compile package assembly
 
 echo "============ running spark =============="
 #run program on cluster
-$SPARK_HOME/bin/spark-submit --master spark://barbaras-mbp.usca.ibm.com:7077 --packages org.elasticsearch:elasticsearch-spark_2.10:2.1.1 --class com.redRock.Boot target/scala-2.10/redRock.jar
-#com.datastax.spark:spark-cassandra-connector_2.10:1.4.0-M2 
+HOSTNAME="$(/bin/hostname -f)"
+$SPARK_HOME/bin/spark-submit --master spark://$HOSTNAME:7077 --packages org.elasticsearch:elasticsearch-spark_2.10:2.1.1 --class com.redRock.Boot target/scala-2.10/redRock.jar
+#com.datastax.spark:spark-cassandra-connector_2.10:1.4.0-M2
