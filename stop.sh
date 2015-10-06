3231f549aa2b66f3f88adc7681f3c5f03e4ecbc5
Luciano Resende <lresende@apache.org>
2015-10-06 09:34:18 -0700

    [RDRK-23] License as Apache License 2.0
    Apply Apache License 2.0 headers and add
    LICENSE and NOTICE files

--- stop.sh
+++ stop.sh
@@ -1,3 +1,19 @@
+#
+# (C) Copyright IBM Corp. 2015, 2015
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 ps aux |grep "com.redRock.Boot"      | tr -s " " |  cut -d " " -f 2 | xargs kill >/dev/null 2>&1
 # ps aux |grep "redrock-server/main.py"     | tr -s " " |  cut -d " " -f 2 | xargs kill >/dev/null 2>&1
 # ps aux |grep "python -m pyspark.daemon"   | tr -s " " |  cut -d " " -f 2 | xargs kill >/dev/null 2>&1
