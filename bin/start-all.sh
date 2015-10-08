738b74635e6096f74ffd65549022ae836a38a28f
barbaragomes <barbaramaltagomes@gmail.com>
2015-10-08 13:37:56 -0700

    [RDRK-39] Creating start-all and stop-all scripts

---
+++ bin/start-all.sh
@@ -0,0 +1,26 @@
+#
+# (C) Copyright IBM Corp. 2015, 2015
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# using environment variable to find RedRock home directory
+if [ -z "$REDROCK_HOME" ]; then echo "REDROCK_HOME is NOT set"; else echo "REDROCK_HOME defined as '$REDROCK_HOME'"; fi
+
+echo "========= Starting Decahose =========="
+
+$REDROCK_HOME/bin/start-decahose.sh
+
+echo "========= Starting REST API =========="
+
+$REDROCK_HOME/bin/start-restapi.sh
