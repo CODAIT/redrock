852ca3aaccd510ff02a346ddd46cd41fa7ebe03f
barbaragomes <barbaramaltagomes@gmail.com>
2015-10-19 13:35:57 -0700

    Do not consider white space for each included and excluded term

--- rest-api/src/main/scala/com/restapi/GetElasticsearchResponse.scala
+++ rest-api/src/main/scala/com/restapi/GetElasticsearchResponse.scala
@@ -38,8 +38,8 @@ class GetElasticsearchResponse(val topTweets: Int, includeTerms:Array[String] = 
 	val baseURL = "http://" + LoadConf.esConf.getString("bindIP") + ":" + LoadConf.esConf.getString("bindPort") + "/" + LoadConf.esConf.getString("indexName") + "/" + esType
 	val searchURL = baseURL + "/_search"
 	val countURL = searchURL + "?search_type=count"
-	val includeTermsES = includeTerms.map(x => s""""$x"""").mkString(",")
-	val excludeTermsES = excludeTerms.map(x => s""""$x"""").mkString(",")
+	val includeTermsES = includeTerms.map(x => s""""${x.trim()}"""").mkString(",")
+	val excludeTermsES = excludeTerms.map(x => s""""${x.trim()}"""").mkString(",")
 
 	def getTopTweetsResponse(): String =
 	{
