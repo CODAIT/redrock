6e392c72ba14bac589634e6bf96f7e7f987cefcb
Luciano Resende <lresende@apache.org>
2016-03-22 10:05:29 -0700

    [RDRK-142] Refactor configuration as part of ApplicationContext
    Configuration is now available as :
    ApplicationContext.Config.appConfig / sparkConfig/ esConfig

--- twitter-powertrack/src/main/scala/com/powertrack/ApplicationContext.scala
+++ twitter-powertrack/src/main/scala/com/powertrack/ApplicationContext.scala
@@ -1,39 +1,38 @@
 /**
- * (C) Copyright IBM Corp. 2015, 2016
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+  * (C) Copyright IBM Corp. 2015, 2016
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  */
 package com.powertrack
 
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
-import org.apache.spark.sql.hive._
-import org.apache.spark.streaming._
-import org.apache.spark.streaming.StreamingContext._
-import org.apache.spark.streaming.dstream.DStream
-import org.elasticsearch.spark._
-import java.io.BufferedInputStream
-import java.io.File
-import java.io.FileInputStream
-import java.io.InputStream
-import org.apache.hadoop.conf._
+import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.hadoop.fs._
-import java.net.URI
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.hive._
 
 
 object ApplicationContext {
+
+  object Config {
+    // Global Application configuration
+    val appConf: Config = ConfigFactory.load("redrock-app").getConfig("redrock")
+    // Spark specific configuration
+    val sparkConf: Config = appConf.getConfig("spark")
+    // Elastic Search configuration
+    val esConf: Config = appConf.getConfig("elasticsearch")
+  }
+
   val sparkConf = new SparkConf()
   sparkConf.setAppName(LoadConf.globalConf.getString("appName") + " - Powertrack")
   sparkConf.set("spark.scheduler.mode", "FAIR")
@@ -48,7 +47,7 @@ object ApplicationContext {
   val sqlContext = new HiveContext(sparkContext)
 
   /* config sqlContext */
-  sqlContext.setConf("spark.sql.shuffle.partitions", s"""${LoadConf.sparkConf.getInt("partitionNumber")}""")  // scalastyle:ignore
+  sqlContext.setConf("spark.sql.shuffle.partitions", s"""${LoadConf.sparkConf.getInt("partitionNumber")}""") // scalastyle:ignore
   sqlContext.setConf("spark.sql.codegen", "true")
 
   /* delete HDFS processed files */
--- twitter-powertrack/src/main/scala/com/powertrack/PrepareTweets.scala
+++ twitter-powertrack/src/main/scala/com/powertrack/PrepareTweets.scala
@@ -1,42 +1,29 @@
 /**
- * (C) Copyright IBM Corp. 2015, 2016
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+  * (C) Copyright IBM Corp. 2015, 2016
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  */
 package com.powertrack
 
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
-import org.apache.spark.sql.{SQLContext, DataFrame, SaveMode}
-import org.apache.spark.streaming._
-import org.apache.spark.streaming.StreamingContext._
-import org.apache.spark.streaming.dstream.DStream
-import org.slf4j.LoggerFactory
-import scala.util.matching.Regex
-import scala.concurrent.{Future, future, Await}
-import scala.concurrent.ExecutionContext.Implicits.global
-import org.apache.spark.rdd.RDD
-import java.io._
-import org.apache.spark.sql.Row
-import org.elasticsearch.spark._
-import org.elasticsearch.spark.sql._
-
-import org.apache.hadoop.fs.{FileSystem, Path, PathFilter}
-import play.api.libs.json._
+import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.{LongWritable, Text}
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SaveMode
+import org.apache.spark.streaming._
+import org.slf4j.LoggerFactory
+import play.api.libs.json._
 
 object PrepareTweets {
   // Extract file names
@@ -44,8 +31,8 @@ object PrepareTweets {
   val logger = LoggerFactory.getLogger(this.getClass)
 
   def startTweetsStreaming(): Unit = {
-    logger.info( s"""Starting Streaming at: ${LoadConf.sparkConf.getString("powertrack.twitterStreamingDataPath")}""") // scalastyle:ignore
-    logger.info( s"""Partition number: ${LoadConf.sparkConf.getInt("partitionNumber")}""")
+    logger.info( s"""Starting Streaming at: ${ApplicationContext.Config.sparkConf.getString("powertrack.twitterStreamingDataPath")}""") // scalastyle:ignore
+    logger.info( s"""Partition number: ${ApplicationContext.Config.sparkConf.getInt("partitionNumber")}""") // scalastyle:ignore
 
     val ssc = createContext()
     logger.info("Starting Streaming")
@@ -58,12 +45,12 @@ object PrepareTweets {
     logger.info("Creating streaming new context")
     // Create the context with a 1 second batch size
     val ssc = new StreamingContext(ApplicationContext.sparkContext,
-      Seconds(LoadConf.sparkConf.getInt("powertrack.streamingBatchTime")))
+      Seconds(ApplicationContext.Config.sparkConf.getInt("powertrack.streamingBatchTime")))
 
     // Filtering file's path in order to avoid _copying files
-    val tweetsStreaming = ssc.fileStream[LongWritable, Text, TextInputFormat](LoadConf.sparkConf.getString("powertrack.twitterStreamingDataPath"), // scalastyle:ignore
+    val tweetsStreaming = ssc.fileStream[LongWritable, Text, TextInputFormat](ApplicationContext.Config.sparkConf.getString("powertrack.twitterStreamingDataPath"), // scalastyle:ignore
       (p: Path) => {
-        if (p.getName().toLowerCase().endsWith(LoadConf.sparkConf.getString("powertrack.fileExtension"))) true else false // scalastyle:ignore
+        if (p.getName().toLowerCase().endsWith(ApplicationContext.Config.sparkConf.getString("powertrack.fileExtension"))) true else false // scalastyle:ignore
       }, true).map(_._2.toString)
     // ssc.textFileStream(LoadConf.sparkConf.getString("powertrack.twitterStreamingDataPath"))
 
@@ -98,15 +85,15 @@ object PrepareTweets {
             s"stringTokenizer(${TweetField.tweet_text}) AS tweet_text_array_tokens")
           .write.mode(SaveMode.Append)
           .format("org.elasticsearch.spark.sql")
-          .options(Map("pushdown" -> "true", "es.nodes" -> LoadConf.esConf.getString("bindIP"),
-          "es.port" -> LoadConf.esConf.getString("bindPort")))
-          .save( s"""${LoadConf.esConf.getString("powertrackIndexName")}/${LoadConf.esConf.getString("esType")}""") // scalastyle:ignore
+          .options(Map("pushdown" -> "true", "es.nodes" -> ApplicationContext.Config.esConf.getString("bindIP"),
+            "es.port" -> ApplicationContext.Config.esConf.getString("bindPort")))
+          .save( s"""${ApplicationContext.Config.esConf.getString("powertrackIndexName")}/${ApplicationContext.Config.esConf.getString("esType")}""") // scalastyle:ignore
       }
       else {
         logger.warn("###### Empty File ######")
       }
 
-      if (LoadConf.sparkConf.getBoolean("powertrack.deleteProcessedFiles")) {
+      if (ApplicationContext.Config.sparkConf.getBoolean("powertrack.deleteProcessedFiles")) {
         // Delete files if they where processed
         logger.info("Deleting File(s):")
         regExp.findAllMatchIn(rdd.toDebugString).foreach((name) => deleteFile(name.toString))
